59b7a303cbfba1e7d30a3edd9c3bf0284f1da774
yuanyang123 <532192498@qq.com>
2017-05-02 21:33:01 +0800

    add a Math class and update func

--- Demo1.java
+++ Demo1.java
@@ -1,7 +1,20 @@
 public class Demo1
 {
-    public static int main(String[] args)
+    public static void main(String[] args)
     {
         System.out.println("Hello Java!");
+        for(int i= 0 ;i<10;i++)
+        {
+            for(int j= 0 ;j<8;j++)
+            {
+            System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        int a = 19,b=22;
+        int c = MathCalc.add(a, b);
+        System.out.println("a+b="+c);
+        return ;
     }
 }
---
+++ MathCalc.java
@@ -0,0 +1,7 @@
+public class MathCalc
+{
+    public static int add(int a,int b)
+    {
+        return a+b;
+    }
+}
